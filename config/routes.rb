1f1e0e8d7df2ec4c01c5d584614a67d4dfe81ad9
tabun_muri <tabun.muri100@gmail.com>
2015-06-25 16:25:51 +0900

    rootパスを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'entrance/index'
+  root 'entrance#index'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
