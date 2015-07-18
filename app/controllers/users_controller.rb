f7e8e72f9dc2fe8fbd52eb5932997d862c8c5a79
tabun_muri <tabun.muri100@gmail.com>
2015-07-18 17:07:30 +0900

    ユーザモデルと、ユーザコントローラに追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new_with_session sign_up_params
+    @user.services.build provider: auth_hash.provider, uid: auth_hash.uid
+
+    if @user.save
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message :notice, :success, kind: 'Twitter' if is_navigational_format?
+    else
+      # ...
+    end
+  end
+
+  private
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up)
+    # params.require(:user).permit(:email, :password)
+  end
+
+  def auth_hash
+    session["devise.twitter_data"]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ActiveRecord::Base
+  has_many :services
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:twitter]
+
+  def self.from_omniauth(auth)
+    ap auth.to_hash
+    service = Service.find_by provider: auth.provider, uid: auth.uid
+
+    # serviceのインスタンス
+    if service
+      service.user
+    else
+      # userのインスタンスを返却する
+      new
+    end
+
+    # # DBにあれば、取得するし、なければ、作る
+    # service = Service.where(provider: auth.provider, uid: auth.uid).first_or_initilize
+    #
+    # # tokenを挿入
+    # service.token = auth.credentials.token
+    #
+    # 新規レコードもしくは、変更がなされた時に更新
+    # service.save if service.new_record? || service.token_changed?
+  end
 end
