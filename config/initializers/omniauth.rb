c6f8a9dde6181a5192936f0ae05e89cbe75b3373
tabun_muri <tabun.muri100@gmail.com>
2015-07-18 13:53:42 +0900

    twitter_keyと、twitter_app_secretを置き換える

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "API_KEY", "API_SECRET"
+  provider :twitter, ENV["TWITTER_APP_ID"], ENV["TWITTER_APP_SECRET"]
 end
