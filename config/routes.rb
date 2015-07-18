bc7120fb874bfef2d86e2e1e3504eaf60aa5953e
tabun_muri <tabun.muri100@gmail.com>
2015-07-18 09:49:55 +0900

    entranceのjsをロードしている箇所を削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'entrance#index'
 
   devise_for :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
